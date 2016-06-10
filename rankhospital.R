4cf6382b0136177c8fa025d9419596523b664fd4
mdgordo <mdgordo@gmail.com>
2016-06-10 19:38:33 -0400

    Update rankhospital.R

--- rankhospital.R
+++ rankhospital.R
@@ -1,3 +1,5 @@
+##answer to part 2
+
 rankhospital <- function(state, outcome, num = "best") {
         ## Read outcome data
         data<-read.csv("outcome-of-care-measures.csv",colClasses = "character")
